c6b534c5b93b917ecd7120885b5a7ab02e2e651f
Henrique da Silva Araujo Neto <47949970+henriquearaujo93@users.noreply.github.com>
2020-02-04 17:20:13 +0000

    update again

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb+srv://henrique:hdsan1993@cluster0-1auso.mongodb.net/tr
 });
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "../../web");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"

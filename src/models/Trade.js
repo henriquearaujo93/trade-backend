108f4fd29e0c0f642f67f0991233220e685480bf
Henrique da Silva Araujo Neto <47949970+henriquearaujo93@users.noreply.github.com>
2020-02-05 00:17:49 +0000

    change date types to string

--- src/models/Trade.js
+++ src/models/Trade.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const TradeSchema = new mongoose.Schema({
     paper: String,
-    entryDate: Date,
-    endDate: Date,
+    entryDate: String,
+    endDate: String,
     openDays: String,
     entryValue: String,
     exitValue: String,

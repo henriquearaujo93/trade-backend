fdba7126b1dad6e10942b8fa19cc45643c52ff7d
Henrique da Silva Araujo Neto <47949970+henriquearaujo93@users.noreply.github.com>
2020-02-05 14:57:55 +0000

    change put to post

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const routes = Router();
 //Trades
 routes.post('/trades/new', TradeController.store);
 routes.delete('/trades/delete/:id', TradeController.destroy);
-routes.put('/trades/edit/:id', TradeController.edit);
+routes.post('/trades/edit/:id', TradeController.edit);
 routes.get('/trades/all', TradeController.list);
 
 module.exports = routes;

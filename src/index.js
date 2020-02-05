7a7da2ee0efe01748be0f53426c8800258546bac
Henrique da Silva Araujo Neto <47949970+henriquearaujo93@users.noreply.github.com>
2020-02-05 14:16:35 +0000

    add new headers

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ mongoose.connect('mongodb+srv://henrique:hdsan1993@cluster0-1auso.mongodb.net/tr
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
